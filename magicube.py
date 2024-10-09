27e074b5491276b89c35dcdea7e807429185a8f2
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-10-09 18:29:34 +0700

    copy implementation

--- magicube.py
+++ magicube.py
@@ -1,5 +1,6 @@
 import random
 import exceptions as ex
+import copy
 
 # Generate a list of numbers from 1 to n, and randomized into a list
 def generate_n_stack(n: int, straight: bool = False):
@@ -59,7 +60,7 @@ class Magicube:
         check_input : list[int] = [x1,y1,z1,x2,y2,z2]
         for i in range(6):
             if check_input[i] < 0 or check_input[i] >= self.size:
-                raise ex.IllegalInputException(f"{chr(ord('x') + (i % 3)).upper()}{i // 3 + 1}")
+                raise ex.IllegalInputException(f"{chr(ord('x') + (i % 3)).upper()}{i // 3 + 1}({check_input[i]})")
 
         #error check finished
         temp : int = self.numbers[z2][y2][x2]
@@ -92,6 +93,16 @@ class Magicube:
             if check_input[i] < 0 or check_input[i] >= self.size:
                 raise ex.IllegalInputException(f"{chr(ord('x') + (i % 3)).upper()}")
         return self.numbers[z][y][x]
+    
+    def __copy__(self):
+        new_cube : Magicube = Magicube(self.size)
+        new_cube.numbers = copy.deepcopy(self.numbers)
+        return new_cube
+        #HERE
+
+def copy_cube(m1 : Magicube, m2 : Magicube):
+    m1.size = m2.size
+    m1.numbers = m2.numbers
 
 true_example = [121,108,7,20,59,31,53,112,109,10,47,61,45,76,86,91,77,71,6,70,25,16,80,104,90,29,28,122,125,11,12,82,34,87,100,107,43,38,33,94,52,64,117,69,13,115,98,4,1,97,51,15,41,124,84,103,3,105,8,96,89,68,63,58,37,30,118,21,123,23,42,111,85,2,75,78,54,99,24,60,113,57,9,62,74,32,93,88,83,19,26,39,92,44,114,66,72,27,102,48,36,110,46,22,101,56,120,55,49,35,40,50,81,65,79,116,17,14,73,95,67,18,119,106,5]
 
