aa8389e00cd3f162b60ff97c8f8c672eebea566b
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-10-09 08:53:46 +0700

    true example and custom cube

--- magicube.py
+++ magicube.py
@@ -20,15 +20,20 @@ def generate_n_stack(n: int, straight: bool = False):
 #X 0 -> +; Y /\ +; Z ke arah kita positif
 
 class Magicube:
-    def __init__(self, cube_size : int = 5, straight = False) -> None:
+    def __init__(self, cube_size : int = 5, straight = False, custom: list[int] = None) -> None:
         #define cube size (n^3)
         self.size : int = cube_size
         
         #generate random state
         self.numbers : list[list[list[int]]] = []
         
+        randstack: list[int]
         #populate cube
-        randstack = generate_n_stack(self.size ** 3, straight)
+        if custom != None:
+            randstack = custom
+        else:
+            randstack = generate_n_stack(self.size ** 3, straight)
+        
         for i in range(self.size):
             temp_j : list[list[int]] = []
             for j in range(self.size):
@@ -54,7 +59,7 @@ class Magicube:
         check_input : list[int] = [x1,y1,z1,x2,y2,z2]
         for i in range(6):
             if check_input[i] < 0 or check_input[i] >= self.size:
-                raise ex.IllegalInputException(i)
+                raise ex.IllegalInputException(f"{chr(ord('x') + (i % 3)).upper()}{i // 3 + 1}")
 
         #error check finished
         temp : int = self.numbers[z2][y2][x2]
@@ -81,13 +86,13 @@ class Magicube:
                 print()
             print()
             
-# test
-test : Magicube = Magicube(5, True)
-try:
-    test.print()
-    test.swap_num(2,125)
-    print("SWAP")
-    test.print()
-    pass
-except ex.IllegalInputException as e:
-    print(e.description)
+    def get(self,x:int,y:int,z:int) -> int:
+        check_input : list[int] = [x,y,z]
+        for i in range(3):
+            if check_input[i] < 0 or check_input[i] >= self.size:
+                raise ex.IllegalInputException(f"{chr(ord('x') + (i % 3)).upper()}")
+        return self.numbers[z][y][x]
+
+true_example = [121,108,7,20,59,31,53,112,109,10,47,61,45,76,86,91,77,71,6,70,25,16,80,104,90,29,28,122,125,11,12,82,34,87,100,107,43,38,33,94,52,64,117,69,13,115,98,4,1,97,51,15,41,124,84,103,3,105,8,96,89,68,63,58,37,30,118,21,123,23,42,111,85,2,75,78,54,99,24,60,113,57,9,62,74,32,93,88,83,19,26,39,92,44,114,66,72,27,102,48,36,110,46,22,101,56,120,55,49,35,40,50,81,65,79,116,17,14,73,95,67,18,119,106,5]
+
+#Magicube(custom=true_example).print()
