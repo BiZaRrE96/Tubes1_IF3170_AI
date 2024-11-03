65a8dcc472a42597250d4c7d1a21519b9d9fd948
AtqiyaHaydar <ashford1524@gmail.com>
2024-11-03 21:18:42 +0700

    feat: add init api

--- back-end/main.py
+++ back-end/main.py
@@ -14,3 +14,27 @@ async def root():
 async def generate_cube(n: int, straight: Optional[bool] = False):
     result = generate_n_stack(n, straight)
     return result
+
+@app.get("/steepest-ascent-hill-climbing")
+async def steepest_ascent_hc():
+    return
+
+@app.get("/stochastic-hill-climbing")
+async def stochastic_hc():
+    return
+
+@app.get("/hill-climbing-with-sideways-move")
+async def hc_with_sideways():
+    return
+
+@app.get("/random-start-hill-climbing")
+async def random_start_hc():
+    return
+
+@app.get("/simulated-annealing")
+async def simulated_annealing():
+    return
+
+@app.get("/genetic-algorithm")
+async def genetic_algorithm():
+    return
