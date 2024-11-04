34f04d2c42382d56b85cbc256dce386a209d66be
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-11-04 18:31:02 +0700

    simulated_annealing standardized output

--- back-end/algorithm/simulated_annealing.py
+++ back-end/algorithm/simulated_annealing.py
@@ -1,7 +1,8 @@
 import random
 import math
-from .utils.magicube import Magicube
+from .utils.Magicubev2 import Magicube2 as Magicube
 from .utils.magicube_adder import fitness, deviation
+from .utils.standard_return import standard_return
 import matplotlib.pyplot as plt
 import time
 
@@ -17,9 +18,42 @@ def generate_neighbor(cube : Magicube):
     Magicube.swap_spot(new_cube, x1, y1, z1, x2, y2, z2)
     return new_cube
 
-def simulated_annealing(cube : Magicube, initial_temp=1000, cooling_rate=0.99995) -> tuple:
+n : int = 5
+
+def simulated_annealing(cubelist : list[int]=None, initial_temp=None, cooling_rate=None) -> dict:
+    
+    #return values
+    start_time = time.time()
+    log : str = ""
+    
+    if cubelist != None:
+        cube: Magicube = Magicube(n,custom=cubelist)
+    else:
+        cube: Magicube = Magicube(n)
+        print("Cube unset, randomizing...")
+        log += "CUBE UNSET\n"
+        
+    if initial_temp != None:
+        pass
+    else:
+        initial_temp = 1000
+        print("Initial temp unset! setting to 1000...")
+        log += "INITIAL TEMP UNSET\n"
+        
+    if cooling_rate != None:
+        pass
+    else:
+        cooling_rate = 0.99995
+        print("cooling rate unset! setting to 0.99995...")
+        log += "INITIAL TEMP UNSET\n"
+
+    #return values
+    first_cube = cube.copy()
+    best_cube : Magicube = None
+    graph : list[float] = [cube.get_fitness()]
+    
     current_cube = cube
-    current_fitness = fitness(cube)
+    current_fitness = cube.get_fitness()
     current_deviation = deviation(cube)
     best_fitness = current_fitness
     best_deviation = current_deviation
@@ -35,27 +69,29 @@ def simulated_annealing(cube : Magicube, initial_temp=1000, cooling_rate=0.99995
     start_time = time.time()
     while temp > 1:
         neighbor_cube = generate_neighbor(current_cube)
-        neighbor_fitness = fitness(neighbor_cube)
+        neighbor_fitness = neighbor_cube.get_fitness()
         neighbor_deviation = deviation(neighbor_cube)
         deltaEF = neighbor_fitness - current_fitness
         deltaED = neighbor_deviation - current_deviation
         exp_term = math.exp(-deltaED / temp)
         
         if neighbor_deviation < current_deviation:
-            current_cube = neighbor_cube
+            current_cube = neighbor_cube.copy()
             current_fitness = neighbor_fitness
             current_deviation = neighbor_deviation
             if current_deviation < best_deviation:
+                best_cube = current_cube.copy()
                 best_fitness = current_fitness
                 best_deviation = current_deviation
             stuck_counter = 0
                 
         else:
             if exp_term > random.random():
-                current_cube = neighbor_cube
+                current_cube = neighbor_cube.copy()
                 current_fitness = neighbor_fitness
                 current_deviation = neighbor_deviation
                 if current_deviation < best_deviation:
+                    best_cube = current_cube.copy()
                     best_fitness = current_fitness
                     best_deviation = current_deviation
             stuck_counter += 1
@@ -73,6 +109,7 @@ def simulated_annealing(cube : Magicube, initial_temp=1000, cooling_rate=0.99995
         iteration += 1
 
         print(f"Iteration: {iteration}, Temperature: {temp}, Fitness: {best_fitness}, Deviation: {best_deviation}")
+        log += (f"Iteration: {iteration}, Temperature: {temp}, Fitness: {best_fitness}, Deviation: {best_deviation}") + "\n"
     end_time = time.time()
 
 
@@ -89,9 +126,8 @@ def simulated_annealing(cube : Magicube, initial_temp=1000, cooling_rate=0.99995
 
     print("RESULT")
     print(f"Best Fitness: {best_fitness}, Best Deviation: {best_deviation}, Iteration: {iteration}, Time: {time_exec}, Stuck Frequency: {stuck_frequency}")
+    log =(f"Best Fitness: {best_fitness}\nBest Deviation: {best_deviation}\nIteration: {iteration}\nTime: {time_exec}\nStuck Frequency: {stuck_frequency}") + log
 
-    return best_fitness, max_fitness_list, avg_fitness_list, exp_term_list, iteration, time_exec, stuck_frequency
+    return standard_return(first_cube,best_cube,max_fitness_list,time_exec,log,{"avg_graph":avg_fitness_list,"exp_graph":exp_term_list,"stuck_freq":stuck_frequency})
+    ##return best_fitness, max_fitness_list, avg_fitness_list, exp_term_list, iteration, time_exec, stuck_frequency
 
-true_example = [121,108,7,20,59,31,53,112,109,10,47,61,45,76,86,91,77,71,6,70,25,16,80,104,90,29,28,122,125,11,12,82,34,87,100,107,43,38,33,94,52,64,117,69,13,115,98,4,1,97,51,15,41,124,84,103,3,105,8,96,89,68,63,58,37,30,118,21,123,23,42,111,85,2,75,78,54,99,24,60,113,57,9,62,74,32,93,88,83,19,26,39,92,44,114,66,72,27,102,48,36,110,46,22,101,56,120,55,49,35,40,50,81,65,79,116,17,14,73,95,67,18,119,106,5]
-cube = Magicube(5)
-simulated_annealing(cube)
